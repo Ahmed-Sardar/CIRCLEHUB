08fd4f6775edf07931c3ffff99225d8d04c8e037
Ahmed Sardar <sardarahmed51103@gmail.com>
2025-12-30 18:01:55 +0530

    fix: move inngest to vercel serverless api route

---
+++ server/api/inngest.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { serve } from "inngest/express";
+import { inngest, functions } from "../inngest/index.js";
+
+export default serve({
+  client: inngest,
+  functions,
+});
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is running'))
-app.use('/api/inngest', serve({ client: inngest, functions }));
+// app.use('/api/inngest', serve({ client: inngest, functions }));
 app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4000;
