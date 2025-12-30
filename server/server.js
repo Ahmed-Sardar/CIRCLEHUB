c1fcd3209d1fb5d1f26d94fb1e3f07788440278a
Ahmed Sardar <sardarahmed51103@gmail.com>
2025-12-30 18:06:14 +0530

    fix: remove express inngest route, use serverless api

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import {inngest, functions} from './inngest/index.js'
-import {serve} from 'inngest/express'
+
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
 
@@ -16,7 +15,7 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is running'))
-// app.use('/api/inngest', serve({ client: inngest, functions }));
+
 app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4000;
